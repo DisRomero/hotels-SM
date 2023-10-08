a7cb0ae7e15abf0568eaa86d6631d7f3ad89f750
Diana Romero <33766030+DisRomero@users.noreply.github.com>
2023-10-07 21:44:50 -0500

    Delete src/back-end directory

--- src/back-end/dataService.js
+++
@@ -1,9 +0,0 @@
-import data from './data.json';
-
-export const getUsers = () => {
-  return data.users;
-};
-
-export const getUserById = (id) => {
-  return data.users.find((user) => user.id === id);
-};
